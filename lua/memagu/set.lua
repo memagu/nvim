08a89ea4134a28b15f8886a8566fd52a12ca4a25
Melker Widén <melker.widen@gmail.com>
2024-09-13 14:15:57 +0200

    Remove force 4-space indent

--- lua/memagu/set.lua
+++ lua/memagu/set.lua
@@ -7,15 +7,15 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    callback = function()
-        vim.opt_local.tabstop = 4
-        vim.opt_local.softtabstop = 4
-        vim.opt_local.shiftwidth = 4
-        vim.opt_local.expandtab = true
-    end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "*",
+--     callback = function()
+--         vim.opt_local.tabstop = 4
+--         vim.opt_local.softtabstop = 4
+--         vim.opt_local.shiftwidth = 4
+--         vim.opt_local.expandtab = true
+--     end,
+-- })
 
 vim.opt.smartindent = true
 
